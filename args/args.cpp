af46ac587379c7be892dab64e7cad42e97ebddbd
Roman Dobias <rom.dobias@gmail.com>
2017-10-08 00:42:17 +0200

    New utility class for argument parsing

---
+++ args/args.cpp
@@ -0,0 +1,165 @@
+#include "args.h"
+#include <iostream>
+Arguments::Arguments()
+{
+	this->argumentPrecedence = OPTIONAL;
+}
+void Arguments::first(argType type)
+{
+	this->argumentPrecedence = type;
+}
+
+
+void Arguments::addFixed(std::string name, std::string format)
+{
+	this->fixedArguments.push_back(std::make_pair(name,format));
+}
+
+void Arguments::addOpt(std::string name, std::string shortKey, bool hasParam, std::string format)
+{
+	auto pair = std::make_pair(name,hasParam);
+	this->options[shortKey] = pair;
+}
+	
+void Arguments::addLongOpt(std::string name, std::string key, std::string shortKey, bool hasParam, std::string format)
+{
+	this->addOpt(name,key,hasParam,format);
+	this->addOpt(name,shortKey,hasParam,format);
+}
+
+int Arguments::parse(int argc, char** argv)
+{
+	// Skip program name
+	char ** argumentPointer = argv+1;
+	
+	enum automata{STEP_FIXED, STEP_OPTIONAL, STEP_END} state;
+
+	state = STEP_OPTIONAL;
+	if(this->argumentPrecedence == FIXED)
+		state = STEP_FIXED;
+
+	while(state != STEP_END)
+	{
+		switch(state)
+		{
+			case STEP_FIXED:
+			for(auto &fixedArg: this->fixedArguments)
+			{
+				// expected fixed, got optional ?
+				if(*argumentPointer == NULL || *argumentPointer[0] == '-')
+				{
+					errorMessage = std::string("Expected fixed argument '")+fixedArg.first+"'";
+					return ARGUMENT_INVALID_FORMAT;
+				}
+				if(!hasFormat(fixedArg.second,*argumentPointer))
+				{
+					errorMessage = std::string("Format mismatch for '")+fixedArg.first+"'";
+					return ARGUMENT_INVALID_FORMAT;
+				}
+				this->arguments[fixedArg.first] = *argumentPointer;
+				argumentPointer++;
+			}
+			state = STEP_END;
+			if(this->argumentPrecedence == FIXED)
+				state = STEP_OPTIONAL;
+			break;
+
+			case STEP_OPTIONAL:
+			while(*argumentPointer != NULL)
+			{
+				if(*argumentPointer[0] == '-')
+				{
+
+					char* omitMinus = (*argumentPointer)+1;
+					char* param = *(argumentPointer+1);
+					if(hasOption(omitMinus))
+					{
+						auto val = &arguments[getOptionName(omitMinus)];
+						if(hasOptionParam(omitMinus))
+						{
+							// expected param
+							if(param == NULL)
+							{
+								errorMessage = std::string("Expected param for option -")+omitMinus;
+								return ARGUMENT_INVALID_FORMAT;
+							}
+							*val = param;
+							++argumentPointer;
+						}
+						else
+							*val = "";
+					} else {
+						errorMessage = std::string("Non-defined option '")+omitMinus+"'";
+						return ARGUMENT_INVALID_FORMAT;
+					}
+					argumentPointer++;
+					continue;
+				}
+				break;
+			}
+			if(*argumentPointer != NULL)
+			{
+				// FIXED arguments went first, optionals were
+				// supposed to end command-line
+				if(this->argumentPrecedence == FIXED)
+				{
+					errorMessage = "Expected any optional argument";
+					return ARGUMENT_INVALID_FORMAT;
+				}
+				else {
+					state = STEP_FIXED;
+					break;
+				}
+			} else {
+				state = STEP_END;
+				if(this->argumentPrecedence == OPTIONAL)
+					state = STEP_FIXED;
+			}
+		}
+	}
+	if(*argumentPointer != NULL)
+	{
+		errorMessage = std::string("Expected no argument, got '")+*argumentPointer;
+		return ARGUMENT_INVALID_FORMAT;
+	}
+	return OK;
+}
+
+
+
+bool Arguments::hasFormat(std::string format, std::string testedWord)
+{
+	// TODO:compare
+	return true;
+}
+
+
+bool Arguments::hasOption(std::string option)
+{
+	return (this->options.find(option) != this->options.end());
+}
+
+bool Arguments::hasOptionParam(std::string longOpt)
+{
+	auto it = this->options.find(longOpt);
+	if(it != this->options.end())
+	{
+		return it->second.second == true;
+	}
+	return false;
+}
+
+std::string Arguments::getOptionName(std::string opt)
+{	
+	auto it = this->options.find(opt);
+	if(it != this->options.end())
+	{
+		return it->second.first;
+	}
+	return "";
+}
+
+std::string Arguments::operator[](std::string argument)
+{
+	return this->arguments[argument];
+}
---
+++ args/args.h
@@ -0,0 +1,64 @@
+#include <vector>
+#include <string>
+#include <map>
+class Arguments
+{
+	public:
+	Arguments();
+	// Type of argument
+	enum argType {
+		FIXED, // cannot be skipped, must be specified by user
+		OPTIONAL // may not be specified by user
+	};
+	enum parseResult {
+		OK = 0,
+		ARGUMENT_INVALID_FORMAT // one of arguments was specified without complying to provided format
+	};
+	enum paramLogic {
+		WITH_PARAM = true,
+		NO_PARAM = false
+	};
+	// Set which parameters (FIXED or OPTIONAL are expected firs)
+	void first(argType type);
+
+	// Specify next fixed argument 'name' with regular language 'format'
+	// NOTE: fixed arguments are expected in the order they were added using addFixed
+	void addFixed(std::string name, std::string format = "");
+
+	// Add optionial argument, which is expected as -K, where K = shortKey
+	void addOpt(std::string name, std::string shortKey, bool hasParam = WITH_PARAM, std::string format = "");
+	
+	// Add optionial argument, which is expected as -K, where K = shortKey or key
+	void addLongOpt(std::string name, std::string key, std::string shortKey, bool hasParam = WITH_PARAM, std::string format = "");
+
+	// Parse provided command-line 
+	int parse(int argc, char** argv);
+
+
+	// Returns value of argument, specified with name 'arg'
+	std::string operator[](const std::string arg);
+
+	std::string getErrorMessage() {return this->errorMessage;}
+	const std::map<std::string,std::string> & getMap() {return this->arguments;}
+	private:
+	// Stores which kind of argument (FIXED/OPT) is expected to appear first
+	// DEFAULT: OPTIONAL
+	argType argumentPrecedence;
+
+	// map argumentName to its value
+	std::map<std::string, std::string> arguments;
+	std::vector<std::pair<std::string,std::string>> fixedArguments;
+	
+	// option => (option name, hasParam)
+	std::map<std::string, std::pair<std::string,bool>> options;
+	//
+	bool hasFormat(std::string format, std::string testedWord);
+
+	// Do we have -s (s = shortOption) in our database ?
+	bool hasOption(std::string option);
+
+	bool hasOptionParam(std::string longOpt);
+	std::string getOptionName(std::string opt);
+
+	std::string errorMessage;
+};
